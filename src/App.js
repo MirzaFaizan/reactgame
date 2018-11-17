c353c729a41cff1a3cf22cbb5117d8f944c643c1
Faizan <mirzafaizanejaz@gmail.com>
2018-11-17 20:22:31 +0500

    embedded bootstrap

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MainFrame from './components/mainFrame';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <MainFrame/>
       </div>
     );
--- src/components/mainFrame.js
+++ src/components/mainFrame.js
@@ -3,20 +3,8 @@ import React, { Component } from 'react';
 class mainFrame extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+       <p className="container">okay removed everything else</p>
       </div>
     );
   }
